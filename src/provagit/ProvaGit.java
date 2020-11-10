c99e888504e0866c55d80409c806d53fb708842f
porretta.christian <porretta.christian@TAVE-PC03.OSDBSESTO.local>
2020-11-10 13:02:59 +0100

    add endless loop

---
+++ src/provagit/ProvaGit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagit;
+
+/**
+ *
+ * @author porretta.christian
+ */
+public class ProvaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double i=2;
+        while(true){
+            System.out.println("hey ciao: "+i);
+            i = i+i;
+        }
+    }
+    
+}
